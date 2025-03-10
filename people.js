c9e2cc2fceda22df786157f39b941230f7effc0f
lorenzo-senettin <senettin.lorenzo54@gmail.com>
2025-03-10 15:19:16 +0100

    add create persone

--- people.js
+++ people.js
@@ -1,3 +1,11 @@
 const fullName = require("./name");
 const hobbies = require("./hobbies");
 
+function createPerson() {
+  return {
+    fullName: fullName("Lorenzo", "Oznerol"),
+    hobbies: hobbies("Ice Skating", "Golf", "Baseball")
+  };
+}
+
+console.log(createPerson());
